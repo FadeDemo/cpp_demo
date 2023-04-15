fc9eeac7a66209c7b467d78764c268980e66de71
paigeman <53284808+paigeman@users.noreply.github.com>
2023-04-15 11:44:33 +0800

    2023/04/15

---
+++ cpp-primer-plus/sample/chapter-3/bondini.cpp
@@ -0,0 +1,15 @@
+//
+// Created by fade on 2023/4/15.
+//
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "\aOperation \"HyperHype\" is now activated!\n";
+    cout << "Enter your agent code:________\b\b\b\b\b\b\b\b";
+    long code;
+    cin >> code;
+    cout << "\aYou entered " << code << "...\n";
+    cout << "\007Code verified! Proceed with Plan Z3!\n";
+    return 0;
+}
