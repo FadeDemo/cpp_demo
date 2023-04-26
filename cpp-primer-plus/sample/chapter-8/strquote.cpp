131f2062bfd4f9a6f7775a5296a484417b60be93
paigeman <53284808+paigeman@users.noreply.github.com>
2023-04-26 16:18:09 +0800

    2023/04/26

---
+++ cpp-primer-plus/sample/chapter-8/strquote.cpp
@@ -0,0 +1,44 @@
+//
+// Created by fade on 2023/4/26.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+string version1(const string &, const string &);
+const string & version2(string &, const string &);
+const string & version3(string &, const string &);
+int main()
+{
+    string input;
+    string copy;
+    string result;
+    cout << "Enter a string: ";
+    getline(cin, input);
+    copy = input;
+    cout << "Your string as entered: " << input << endl;
+    result = version1(input, "***");
+    cout << "Your string enhanced: " << result << endl;
+    cout << "Your original string: " << input << endl;
+    result = version2(input, "###");
+    cout << "Your string enhanced: " << result << endl;
+    cout << "Your original string: " << input << endl;
+    cout << "Resetting original string.\n";
+    input = copy;
+    result = version3(input, "@@@");
+    cout << "Your string enhanced: " << result << endl;
+    cout << "Your original string: " << input << endl;
+    return 0;
+}
+string version1(const string & s1, const string & s2) {
+    string temp;
+    temp = s2 + s1 + s2;
+    return temp;
+}
+const string & version2(string & s1, const string & s2) {
+    return (s1 = s2 + s1 + s2);
+}
+const string & version3(string & s1, const string & s2) {
+    // fixme 会导致悬空引用
+    string temp;
+    return temp = s2 + s1 + s2;
+}
