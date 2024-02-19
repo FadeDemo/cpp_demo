1e888c35cf70e19257f00cbbc16f2271de4b0111
paigeman <53284808+paigeman@users.noreply.github.com>
2024-02-19 12:18:24 +0800

    2024-02-18

---
+++ cpp-templates/basics/actualparameter.cpp
@@ -0,0 +1,66 @@
+//
+// Created by fade on 2024/2/19.
+//
+#include <list>
+
+template <typename T>
+class List {
+
+};
+
+typedef struct {
+    double x, y, z;
+} Point;
+
+typedef enum { red, green, blue } * ColorPtr;
+
+template<typename T, T nontype_param>
+class C {};
+
+class X {
+public:
+    int n;
+};
+
+X x;
+
+//C<int *, &x.n> c;
+C<int X::*, &X::n> c1;
+
+template<typename T>
+void templ_func();
+
+C<void(), &templ_func<double>> c5;
+C<void (*)(), &templ_func<double>> c6;
+C<int *, nullptr> c7;
+
+class Derived : public X {
+
+} derived_obj;
+
+//C<X *, &derived_obj> err1;
+
+//C<int&, x.n> err2;
+
+int a[10];
+C<int *, &a[0]> err3; // was treated as the address of the array?
+
+template<typename T1, typename T2, template<typename> class Container>
+class Relation {
+    Container<T1> dom1;
+    Container<T2> dom2;
+};
+
+Relation<int, double, std::list> rel;
+
+int main()
+{
+    struct Association {
+        int * p;
+        int * q;
+    };
+    List<Association*> error1;
+    List<ColorPtr> error2;
+    List<Point > ok;
+    return 0;
+}
