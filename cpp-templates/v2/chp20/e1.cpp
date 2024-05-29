ac638fc93ebde079066edb36c69c259f0147ea77
paigeman <53284808+paigeman@users.noreply.github.com>
2024-05-29 13:31:31 +0800

    2024-05-29

---
+++ cpp-templates/v2/chp20/e1.cpp
@@ -0,0 +1,16 @@
+//
+// Created by fade on 2024/5/29.
+//
+class A {
+
+};
+
+class B : public A {
+
+};
+
+int main()
+{
+//    A a = B();
+    return 0;
+}
---
+++ cpp-templates/v2/chp20/e2.cpp
@@ -0,0 +1,10 @@
+//
+// Created by fade on 2024/5/29.
+//
+int test() {
+
+}
+
+void test() {
+
+}
